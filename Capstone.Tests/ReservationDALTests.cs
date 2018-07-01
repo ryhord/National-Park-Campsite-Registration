75a76127b77bf12674f11c60123144e19a2dcfe5
Ryan Hord <ryhord13@gmail.com>
2018-07-01 10:52:57 -0400

    started reservation integration tests

---
+++ Capstone.Tests/ReservationDALTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Capstone.DAL;
+using Capstone.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Tests
+{
+	[TestClass]
+	public class ReservationDAL_Tests : CampgroundDBTests
+	{
+
+		private ReservationDAL dal = new ReservationDAL(ConnectionString);
+
+		[TestMethod]
+		public void CreateReservation_Test()
+		{
+			Reservation res = new Reservation();
+			res.ReservationId = 60;
+			res.SiteId = 6;
+			res.Name = "Ryan";
+			res.FromDate = Convert.ToDateTime("2019-06-20");
+			res.ToDate = Convert.ToDateTime("2019-06-23");
+
+			Assert.AreEqual(60, dal.CreateReservation(res));
+
+
+
+		}
+	}
+}

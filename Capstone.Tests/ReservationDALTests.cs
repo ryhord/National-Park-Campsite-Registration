144b930731457dd90350a0c780f9af5a4b8a6474
Ryan Hord <ryhord13@gmail.com>
2018-07-02 16:24:02 -0400

    completed SiteDAL Test

--- Capstone.Tests/ReservationDALTests.cs
+++ Capstone.Tests/ReservationDALTests.cs
@@ -94,5 +94,86 @@ namespace Capstone.Tests
 			Assert.AreEqual((initialNumberOfReservations + 1), finalNumberOfReservations);
 		}
 
+		[TestMethod]
+		public void CreateReservation_Fail_Test()
+		{
+			Reservation res = new Reservation();
+			res.SiteId = 6;
+			res.Name = "Ryan";
+			res.FromDate = Convert.ToDateTime("2018-06-20");
+			res.ToDate = Convert.ToDateTime("2018-06-25");
+
+			int initialNumberOfReservations = 0;
+			int finalNumberOfReservations = 0;
+
+			try
+			{
+				// Create a connection
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+
+					SqlCommand check = new SqlCommand("SELECT COUNT(reservation_id) AS numOfReservations FROM reservation " +
+						"WHERE ((reservation.from_date BETWEEN @fromDate AND @toDate) " +
+						"OR (reservation.to_date BETWEEN @fromDate AND @toDate)) " +
+						"AND site_id = @site_id " +
+						"GROUP BY reservation_id;", conn);
+					check.Parameters.AddWithValue("@fromDate", res.FromDate);
+					check.Parameters.AddWithValue("@toDate", res.ToDate);
+					check.Parameters.AddWithValue("@site_id", res.SiteId);
+
+					SqlDataReader reader = check.ExecuteReader();
+
+					while (reader.Read())
+					{
+						initialNumberOfReservations = Convert.ToInt32(reader["numOfReservations"]);
+					}
+
+
+					reader.Close();
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			dal.CreateReservation(res);
+
+			try
+			{
+				// Create a connection
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+
+					SqlCommand check = new SqlCommand("SELECT COUNT(reservation_id) AS numOfReservations FROM reservation " +
+						"WHERE ((reservation.from_date BETWEEN @fromDate AND @toDate) " +
+						"OR (reservation.to_date BETWEEN @fromDate AND @toDate)) " +
+						"AND site_id = @site_id " +
+						"GROUP BY reservation_id;", conn);
+					check.Parameters.AddWithValue("@fromDate", res.FromDate);
+					check.Parameters.AddWithValue("@toDate", res.ToDate);
+					check.Parameters.AddWithValue("@site_id", res.SiteId);
+
+					SqlDataReader reader = check.ExecuteReader();
+
+					while (reader.Read())
+					{
+						finalNumberOfReservations = Convert.ToInt32(reader["numOfReservations"]);
+					}
+
+
+					reader.Close();
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			Assert.AreEqual(initialNumberOfReservations, finalNumberOfReservations);
+		}
+
 	}
 }
--- Capstone.Tests/SiteDALTests.cs
+++ Capstone.Tests/SiteDALTests.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using Capstone.DAL;
+using Capstone.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Capstone.Tests
 {
 	[TestClass]
-	public class UnitTest1
+	public class SiteDALTests : CampgroundDBTests
 	{
+		private SiteDAL dal = new SiteDAL(ConnectionString);
+
 		[TestMethod]
-		public void TestMethod1()
+		public void GetAvailableSitesByCampground_Test()
 		{
+
+			int campgroundId = 1;
+			string fromDate = "2018-08-10";
+			string toDate = "2018-08-15";
+			int numOfDays = 5;
+			List<Site> availableSites = dal.GetAvailableSitesByCampground(campgroundId, fromDate, toDate, numOfDays);
+			// expected 1 result
+			int actualAvailableSites = availableSites.Count;
+			int expectedAvailableSites = 2;
+			Assert.AreEqual(expectedAvailableSites, actualAvailableSites);
 		}
 	}
 }

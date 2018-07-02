abc81405106b9b266f4ef226cfe66b8a5ebeb0c4
Ryan Hord <ryhord13@gmail.com>
2018-07-02 13:52:21 -0400

    CreateReservation Test fixed

--- Capstone.Tests/ReservationDALTests.cs
+++ Capstone.Tests/ReservationDALTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using Capstone.DAL;
 using Capstone.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -20,10 +21,77 @@ namespace Capstone.Tests
 			res.FromDate = Convert.ToDateTime("2019-06-20");
 			res.ToDate = Convert.ToDateTime("2019-06-23");
 
-			int resId = dal.CreateReservation(res);
-			int expectedId = 7;
+			int initialNumberOfReservations = 0;
+			int finalNumberOfReservations = 0;
 
-			Assert.AreEqual(expectedId, resId);
+			try
+			{
+				// Create a connection
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+
+					SqlCommand check = new SqlCommand("SELECT COUNT(reservation_id) AS numOfReservations FROM reservation " +
+						"WHERE ((reservation.from_date BETWEEN @fromDate AND @toDate) " +
+						"OR (reservation.to_date BETWEEN @fromDate AND @toDate)) " +
+						"AND site_id = @site_id " +
+						"GROUP BY reservation_id;", conn);
+					check.Parameters.AddWithValue("@fromDate", res.FromDate);
+					check.Parameters.AddWithValue("@toDate", res.ToDate);
+					check.Parameters.AddWithValue("@site_id", res.SiteId);
+
+					SqlDataReader reader = check.ExecuteReader();
+
+					while (reader.Read())
+					{
+						initialNumberOfReservations = Convert.ToInt32(reader["numOfReservations"]);
+					}
+
+
+					reader.Close();
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+
+			dal.CreateReservation(res);
+
+			try
+			{
+				// Create a connection
+				using (SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+
+					SqlCommand check = new SqlCommand("SELECT COUNT(reservation_id) AS numOfReservations FROM reservation " +
+						"WHERE ((reservation.from_date BETWEEN @fromDate AND @toDate) " +
+						"OR (reservation.to_date BETWEEN @fromDate AND @toDate)) " +
+						"AND site_id = @site_id " +
+						"GROUP BY reservation_id;", conn);
+					check.Parameters.AddWithValue("@fromDate", res.FromDate);
+					check.Parameters.AddWithValue("@toDate", res.ToDate);
+					check.Parameters.AddWithValue("@site_id", res.SiteId);
+
+					SqlDataReader reader = check.ExecuteReader();
+
+					while (reader.Read())
+					{
+						finalNumberOfReservations = Convert.ToInt32(reader["numOfReservations"]);
+					}
+
+
+					reader.Close();
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			Assert.AreEqual((initialNumberOfReservations + 1), finalNumberOfReservations);
 		}
 
 	}
